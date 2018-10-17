2c42c734910d1ec09e1e0fb91dcea019e2aeea14
Daniyar <mrtusup@gmail.com>
2018-10-17 22:45:16 +0600

    Add BottomSheetBehavior

--- app/src/main/java/kz/careme/android/modules/MainActivity.java
+++ app/src/main/java/kz/careme/android/modules/MainActivity.java
@@ -2,11 +2,26 @@ package kz.careme.android.modules;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import kz.careme.android.R;
+import kz.careme.android.modules.kids.MyKidsFragment;
 import kz.careme.android.modules.login.ChooseLoginActivity;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView mBottomNavigationView;
+
+    private FragmentManager mFragmentManager;
+    private Fragment mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,43 @@ public class MainActivity extends BaseActivity {
         if (profiler.getName() == null || profiler.getName().isEmpty()) {
             startActivity(new Intent(this, ChooseLoginActivity.class));
             finish();
+            return;
         }
+        ButterKnife.bind(this);
+        mFragmentManager = getSupportFragmentManager();
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_my_kids:
+                        showFragment(MyKidsFragment.TAG);
+                        break;
+                }
+                return true;
+            }
+        });
+        mBottomNavigationView.setSelectedItemId(R.id.action_my_kids);
+
+    }
+
+    private void showFragment(String tag) {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        Fragment fragment = mFragmentManager.findFragmentByTag(tag);
+        if (mCurrentFragment == fragment) {
+            return;
+        }
+        if (fragment == null) {
+            switch (tag) {
+                default:
+                    fragment = MyKidsFragment.newInstance();
+                    transaction.add(R.id.bottom_sheet_behavior_content, fragment, tag);
+                    break;
+            }
+        }
+        if (mCurrentFragment != null) {
+            transaction.hide(mCurrentFragment);
+        }
+        transaction.show(fragment).commit();
+        mCurrentFragment = fragment;
     }
 }
---
+++ app/src/main/java/kz/careme/android/modules/kids/MyKidsFragment.java
@@ -0,0 +1,46 @@
+package kz.careme.android.modules.kids;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kz.careme.android.R;
+
+public class MyKidsFragment extends Fragment {
+    public static final String TAG = "MyKidsFragment";
+
+    public MyKidsFragment() {
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static MyKidsFragment newInstance() {
+        MyKidsFragment fragment = new MyKidsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_kids, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
