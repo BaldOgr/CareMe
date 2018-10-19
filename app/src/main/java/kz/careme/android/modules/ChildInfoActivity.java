4deeef77e4261f2dfe4140026c63b96253b02e60
Daniyar <mrtusup@gmail.com>
2018-10-19 11:58:37 +0600

    ChildInfoActivity

---
+++ app/src/main/java/kz/careme/android/modules/ChildInfoActivity.java
@@ -0,0 +1,29 @@
+package kz.careme.android.modules;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import kz.careme.android.R;
+
+public class ChildInfoActivity extends BaseActivity {
+
+    @BindView(R.id.image)
+    ImageView mChildImage;
+
+    @BindView(R.id.child_first_name)
+    TextView mChildFirstName;
+
+    @BindView(R.id.child_second_name)
+    TextView mChildSecondName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_info);
+        ButterKnife.bind(this);
+
+    }
+}
