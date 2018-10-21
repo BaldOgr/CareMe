77937efaf24413c25cb5a1f8fcf588b1d648dde7
Daniyar <mrtusup@gmail.com>
2018-10-21 18:59:01 +0600

    add chips

--- app/src/main/java/kz/careme/android/modules/chat/ChatActivity.java
+++ app/src/main/java/kz/careme/android/modules/chat/ChatActivity.java
@@ -1,18 +1,25 @@
 package kz.careme.android.modules.chat;
 
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
+import android.support.design.chip.Chip;
+import android.support.design.chip.ChipGroup;
+import android.support.design.resources.TextAppearance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import kz.careme.android.R;
+import kz.careme.android.modules.BaseActivity;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends BaseActivity {
 
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
@@ -23,15 +30,31 @@ public class ChatActivity extends AppCompatActivity {
     @BindView(R.id.message)
     EditText mMessage;
 
+    @BindView(R.id.chips)
+    ChipGroup chipGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         ButterKnife.bind(this);
-        mMessage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-            }
-        });
+
+        for (int i = 0; i < 10; i++) {
+            Chip chip = new Chip(this);
+            chip.setText("asdadsad");
+            chipGroup.addView(chip, i, new ChipGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        }
+
+    }
+
+    @OnClick(R.id.expand_chips)
+    public void onExpandButtonClick(View v) {
+        if (chipGroup.isSingleSelection()) {
+            v.animate().rotation(180).setDuration(150).start();
+            chipGroup.setSingleSelection(false);
+        } else {
+            v.animate().rotation(0).setDuration(150).start();
+            chipGroup.setSingleSelection(true);
+        }
     }
 }
